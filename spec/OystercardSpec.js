81cf9583d287c17cc316db84f5187971372fcdf9
Kirk Smith <kirks@hotmail.co.uk>
2016-07-28 00:46:06 +0100

    refactor test

--- spec/OystercardSpec.js
+++ spec/OystercardSpec.js
@@ -1,17 +1,16 @@
 describe ('Oystercard', function(){
-  var oystercard;
+  var oystercard = new Oystercard();
+
 
   describe('#initialize', function(){
 
     it('will have a balance of 0', function(){
-      oystercard = new Oystercard();
       expect(oystercard.balance).toEqual(0);
     });
   });
 
   describe('#topUp', function(){
     it('will add top amount to balance', function(){
-      oystercard = new Oystercard();
       oystercard.topUp(5);
       expect(oystercard.balance).toEqual(5);
     });
